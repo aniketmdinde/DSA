2f84f7f34b24ce09e1f3685a12ec5de9bee711bb
aniketmdinde <aniketmdinde@gmail.com>
2024-08-23 17:37:14 +0530

    Bit Manipulation - GetIthBit

---
+++ 7.Bit Manipulation/2.EvenOdd/EvenOdd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    if (x & 1)
+        cout << "Odd" << endl;
+    else
+        cout << "Even" << endl;
+
+    return 0;
+}
