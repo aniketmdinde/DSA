ca83de613d65140df1ea8637221b3296c539df37
aniketmdinde <aniketmdinde@gmail.com>
2024-08-26 06:19:39 +0530

    Recursion - Fibonacci

---
+++ 8.Recursion/3.Fibonacci/Fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n == 0 or n == 1)
+        return n;
+
+    int f1 = fibonacci(n - 1);
+    int f2 = fibonacci(n - 2);
+
+    return f1 + f2;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << fibonacci(n) << endl;
+
+    return 0;
+}
