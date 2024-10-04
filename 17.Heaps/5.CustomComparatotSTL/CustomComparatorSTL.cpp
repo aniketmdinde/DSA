646d07a6bcd92f2a336a540a1bcc199915d5ad7e
aniketmdinde <aniketmdinde@gmail.com>
2024-10-04 10:28:52 +0530

    Heaps CustomComparator

---
+++ 17.Heaps/5.CustomComparatotSTL/CustomComparatorSTL.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Compare
+{
+public:
+    bool operator()(int a, int b)
+    {
+        // return a > b; // MAX HEAP
+        return a < b; // MIN HEAP
+    }
+};
+
+int main()
+{
+    int arr[] = {10, 15, 20, 13, 6, 90};
+    int n = sizeof(arr) / sizeof(int);
+
+    priority_queue<int, vector<int>, Compare> heap; // using custom comparator
+
+    for (int x : arr)
+    {
+        heap.push(x);
+    }
+
+    while (!heap.empty())
+    {
+        cout << heap.top() << " ";
+        heap.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
